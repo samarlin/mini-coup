90645b473bb74bd67d890391acd40ec44c43b511
samarlin <sam@remap.ucla.edu>
2021-02-04 08:45:08 -0500

    minor changes after playing last night

--- client/src/pages/Home.svelte
+++ client/src/pages/Home.svelte
@@ -68,8 +68,8 @@
 
 <main>
     <h1>Welcome to Mini Coup!</h1>
-    <p>Select an option below to either join or start a game.</p>
-    <p>If a friend of yours has already created a room, they can simply send you a link to it; otherwise, you can join by entering the room's number.</p>
+    <p>Select an option below to either join or start a game.</p><br>
+    <p>If a friend of yours has already created a room, they can simply send you a link to it; otherwise, you can join it by entering the room's number.</p>
     <br><br>
     <button on:click={createLobby}>Create New Room</button>
     <button on:click={joinLobby_popup}>Join Existing Room</button>
