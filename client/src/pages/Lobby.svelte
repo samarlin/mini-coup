e0160d6b271957dc295918f6ef1cce7ae0bc6212
samarlin <sam@remap.ucla.edu>
2021-02-04 17:24:09 -0500

    ...two in a row

--- client/src/pages/Lobby.svelte
+++ client/src/pages/Lobby.svelte
@@ -73,7 +73,7 @@
 		popup_attr.message = (error) ? "Name already in use, try again:" : "Enter your name:";
 		popup_attr.onSubmit = (name) => {player_name = name; popup_attr = popup.initialData();};
         popup_attr.display = true;
-        popup_attr = popup.attr;
+        popup_attr = popup_attr;
     }
 
     function checkName() {
